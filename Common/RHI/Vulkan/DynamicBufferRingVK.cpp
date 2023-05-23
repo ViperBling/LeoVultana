217ced3c15117367a65d0b9dabce53991121dcfe
ViperBling <qiusongtao2010@hotmail.com>
2023-05-24 00:14:06 +0800

    [M] DynamicBufferRing Update

--- Common/RHI/Vulkan/DynamicBufferRingVK.cpp
+++ Common/RHI/Vulkan/DynamicBufferRingVK.cpp
@@ -1,4 +1,4 @@
-#include "PCHVK.h"
+﻿#include "PCHVK.h"
 #include "DynamicBufferRingVK.h"
 #include "Misc.h"
 #include "ExtDebugUtilsVK.h"
@@ -12,24 +12,34 @@ void DynamicBufferRing::OnCreate(Device *pDevice, uint32_t numberOfBackBuffers, 
     mMemTotalSize = memTotalSize;
     mMem.OnCreate(numberOfBackBuffers, mMemTotalSize);
 
-#ifdef USE_VMA
-    VkBufferCreateInfo bufferCI{ VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+//#ifdef USE_VMA
+//    VkBufferCreateInfo bufferCI{ VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+//    bufferCI.size = mMemTotalSize;
+//    bufferCI.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+//
+//    VmaAllocationCreateInfo vmaAllocationCI{};
+//    vmaAllocationCI.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+//    vmaAllocationCI.flags = VMA_ALLOCATION_CREATE_USER_DATA_COPY_STRING_BIT;
+//    vmaAllocationCI.pUserData = name;
+//
+//    VK_CHECK_RESULT(vmaCreateBuffer(
+//        pDevice->GetAllocator(), &bufferCI,
+//        &vmaAllocationCI, &mBuffer, &mBufferAllocation,nullptr));
+//    SetResourceName(pDevice->GetDevice(), VK_OBJECT_TYPE_BUFFER, (uint64_t)mBuffer, "DynamicBufferRing");
+//    VK_CHECK_RESULT(vmaMapMemory(pDevice->GetAllocator(), mBufferAllocation, (void**)&m_pData));
+//#else
+    // 创建一个可以保存uniform、顶点、索引的Buffer
+    VkBufferCreateInfo bufferCI{};
+    bufferCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferCI.pNext = nullptr;
+    bufferCI.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |  VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
     bufferCI.size = mMemTotalSize;
-    bufferCI.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-
-    VmaAllocationCreateInfo vmaAllocationCI{};
-    vmaAllocationCI.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
-    vmaAllocationCI.flags = VMA_ALLOCATION_CREATE_USER_DATA_COPY_STRING_BIT;
-    vmaAllocationCI.pUserData = name;
-
-    VK_CHECK_RESULT(vmaCreateBuffer(
-        pDevice->GetAllocator(), &bufferCI,
-        &vmaAllocationCI, &mBuffer, &mBufferAllocation,nullptr));
-    SetResourceName(pDevice->GetDevice(), VK_OBJECT_TYPE_BUFFER, (uint64_t)mBuffer, "DynamicBufferRing");
-    VK_CHECK_RESULT(vmaMapMemory(pDevice->GetAllocator(), mBufferAllocation, (void**)&m_pData));
-#else
-
-#endif
+    bufferCI.queueFamilyIndexCount = 0;
+    bufferCI.pQueueFamilyIndices = nullptr;
+    bufferCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    bufferCI.flags = 0;
+    VK_CHECK_RESULT(vkCreateBuffer(m_pDevice->GetDevice(), &bufferCI, nullptr, &mBuffer));
+//#endif
 }
 
 void DynamicBufferRing::OnDestroy()

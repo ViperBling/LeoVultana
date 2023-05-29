2c8366e131475cd488d7c3d4b11f5cefe1f505fb
qiusongtao <qiusongtao121731@pwrd.com>
2023-05-29 18:46:40 +0800

    [M] Update GLTFDepthPassVK

--- Common/RHI/Vulkan/GLTF_VK/GLTFDepthPassVK.cpp
+++ Common/RHI/Vulkan/GLTF_VK/GLTFDepthPassVK.cpp
@@ -1,5 +1,55 @@
-//
-// Created by Administrator on 2023/5/12.
-//
-
+#include "PCHVK.h"
 #include "GLTFDepthPassVK.h"
+#include "GLTFHelpersVK.h"
+#include "ShaderCompilerHelperVK.h"
+#include "ExtDebugUtilsVK.h"
+#include "ResourceViewHeapsVK.h"
+#include "HelperVK.h"
+#include "Async.h"
+#include "GLTFPBRMaterial.h"
+
+using namespace LeoVultana_VK;
+
+void GLTFDepthPass::OnCreate(
+    Device *pDevice,
+    VkRenderPass renderPass,
+    UploadHeap *pUploadHeap,
+    ResourceViewHeaps *pHeaps,
+    DynamicBufferRing *pDynamicBufferRing,
+    StaticBufferPool *pStaticBufferPool,
+    GLTFTexturesAndBuffers *pGLTFTexturesAndBuffers,
+    AsyncPool *pAsyncPool)
+{
+
+}
+
+void GLTFDepthPass::OnDestroy()
+{
+
+}
+
+GLTFDepthPass::PerFrame *GLTFDepthPass::SetPerFrameConstants()
+{
+    return nullptr;
+}
+
+void GLTFDepthPass::Draw(VkCommandBuffer cmdBuffer)
+{
+
+}
+
+void GLTFDepthPass::CreateDescriptors(
+    int inverseMatrixBufferSize,
+    DefineList *pAttributeDefines,
+    DepthPrimitives *pPrimitives)
+{
+
+}
+
+void GLTFDepthPass::CreatePipeline(
+    std::vector<VkVertexInputAttributeDescription> layout,
+    const DefineList &defineList,
+    DepthPrimitives *pPrimitives)
+{
+
+}
--- Common/RHI/Vulkan/GLTF_VK/GLTFDepthPassVK.h
+++ Common/RHI/Vulkan/GLTF_VK/GLTFDepthPassVK.h
@@ -1,14 +1,81 @@
-//
-// Created by Administrator on 2023/5/12.
-//
+#pragma once
 
-#ifndef LEOVULTANA_GLTFDEPTHPASS_H
-#define LEOVULTANA_GLTFDEPTHPASS_H
+#include "GLTFTexturesAndBuffersVK.h"
 
+namespace LeoVultana_VK
+{
+    struct DepthMaterial
+    {
+        int mTextureCount = 0;
+        VkDescriptorSet mDescSet{};
+        VkDescriptorSetLayout mDescSetLayout{};
 
-class GLTFDepthPass {
+        DefineList mDefines;
+        bool mDoubleSided = false;
+    };
 
-};
+    struct DepthPrimitives
+    {
+        Geometry mGeometry;
+        DepthMaterial* m_pMaterial = nullptr;
 
+        VkPipeline mPipeline{};
+        VkPipelineLayout mPipelineLayout{};
 
-#endif //LEOVULTANA_GLTFDEPTHPASS_H
+        VkDescriptorSet mDescSet{};
+        VkDescriptorSetLayout mDescSetLayout{};
+    };
+
+    struct DepthMesh
+    {
+        std::vector<DepthPrimitives> mPrimitives;
+    };
+
+    class GLTFDepthPass
+    {
+    public:
+        struct PerFrame
+        {
+            math::Matrix4 mViewProj;
+        } mPerFrame;
+        struct PerObject
+        {
+            math::Matrix4 mWorld;
+        } mPerObject;
+
+    public:
+        void OnCreate(
+            Device* pDevice,
+            VkRenderPass renderPass,
+            UploadHeap* pUploadHeap,
+            ResourceViewHeaps *pHeaps,
+            DynamicBufferRing *pDynamicBufferRing,
+            StaticBufferPool *pStaticBufferPool,
+            GLTFTexturesAndBuffers *pGLTFTexturesAndBuffers,
+            AsyncPool *pAsyncPool = nullptr);
+
+        void OnDestroy();
+        GLTFDepthPass::PerFrame* SetPerFrameConstants();
+        void Draw(VkCommandBuffer cmdBuffer);
+
+    private:
+        void CreateDescriptors(int inverseMatrixBufferSize, DefineList* pAttributeDefines, DepthPrimitives* pPrimitives);
+        void CreatePipeline(std::vector<VkVertexInputAttributeDescription> layout, const DefineList& defineList, DepthPrimitives* pPrimitives);
+
+    private:
+        Device*                 m_pDevice;
+        VkRenderPass            mRenderPass{};
+        VkSampler               mSampler{};
+        VkDescriptorBufferInfo  mPerFrameDesc;
+
+        ResourceViewHeaps*      m_pResourceViewHeaps;
+        DynamicBufferRing*      m_pDynamicBufferRing;
+        StaticBufferPool*       m_pStaticBufferPool;
+
+        std::vector<DepthMesh>      mMeshes;
+        std::vector<DepthMaterial>  mMaterialDatas;
+        DepthMaterial               mDefaultMaterial;
+
+        GLTFTexturesAndBuffers*     m_pGLTFTexturesAndBuffers;
+    };
+}
